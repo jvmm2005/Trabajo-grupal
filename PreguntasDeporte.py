933c24e6e86d5b90708adbfafd4ac8d08f7adfbe
Nicolasolgoso <145667874+Nicolasolgoso@users.noreply.github.com>
2023-11-21 18:53:10 +0100

    Create PreguntasDeporte.py

---
+++ PreguntasDeporte.py
@@ -0,0 +1,14 @@
+import random
+from ClasePreguntas import preguntas
+sports_question = [
+    preguntas("¿De qué deporte se han tomado expresiones como tirar la toalla o salvado por la campana?", "Boxeo", "Fútbol", "Baloncesto", "Golf"),
+    preguntas("¿Cuál es el deporte que se juega en un campo con nueve jugadores por equipo?", "Béisbol", "Fútbol", "Baloncesto", "Hockey"),
+    preguntas("¿Cuál es el deporte más practicado en el mundo? ", "Natación", "Fútbol americano", "Fútbol", "Ping pong"),
+    preguntas("¿Quién se considera el mejor jugador de baloncesto de todos los tiempos?", "Michael Jordan", "Lebron James", "Kobe Bryant", "Stephen Curry"),
+    preguntas("¿En qué país se originó el deporte del rugby?", "Inglaterra", "Estados Unidos", "Australia", "Nueva Zelanda"),
+    preguntas("¿Quien es el deportista mejor pagado de España en 2023?", "Jon Ram", "Koke", "Rafa Nadal", "Carlos Alcaraz"),
+    preguntas("¿Cuántos anillos de la NBA ha conseguido Shaquille O’Neal?", "4", "2", "3", "5"),
+    preguntas("¿Cuántos Grand Slam tiene Roger Federer?", "20", "15", "17", "12"),
+    preguntas("¿Cuál es la última cinta que puedes conseguir en las artes marciales?", "La negra", "La roja", "La blanca", "La azul"),
+    preguntas("¿Cada cuántos años se celebran los Juegos Olímpicos?", "4", "2", "3", "6"),
+]
