dff1cf78e1d730a5f3dbca8a8c3666cec62f084c
manuelgutierrezcarranza <145667837+manuelgutierrezcarranza@users.noreply.github.com>
2023-11-21 18:33:31 +0100

    history_cuestions.py

--- history_cuestions.py
+++ history_cuestions.py
@@ -1,15 +1,15 @@
 import random
 from ClasePreguntas import preguntas
 history_cuestions = [
-    preguntas("In what year did the First World War begin?", "1914", "1920", "1905", "1932")
-    preguntas("Who was the first President of the United States?", "Thomas Jefferson", "George Washington", "Abraham Lincoln", "John Adams")
-    preguntas("Which civilization built the pyramids of Giza?", "Mesopotamia", "Rome", "Egypt", "Greece")
-    preguntas("In what year was the Declaration of Independence of the United States signed?", "1776", "1789", "1800", "1765")
-    preguntas("Who was the leader of the Russian Revolution in 1917?", "Joseph Stalin", "Vladimir Lenin", "Leon Trotsky", "Nikita Khrushchev")
-    preguntas("Which battle ended the American Civil War?", "Battle of Gettysburg", "Battle of Antietam", "Battle of Bull Run", "Battle of Appomattox")
-    preguntas("When did the French Revolution take place?", "1776", "1789", "1848", "1765")
-    preguntas("What was the ancient capital of the Roman Empire?", "Athens", "Rome", "Constantinople", "Paris")
-    preguntas("In what year was the League of Nations founded?", "1919", "1925", "1933", "1908")
+    preguntas("In what year did the First World War begin?", "1914", "1920", "1905", "1932"),
+    preguntas("Who was the first President of the United States?", "Thomas Jefferson", "George Washington", "Abraham Lincoln", "John Adams"),
+    preguntas("Which civilization built the pyramids of Giza?", "Mesopotamia", "Rome", "Egypt", "Greece"),
+    preguntas("In what year was the Declaration of Independence of the United States signed?", "1776", "1789", "1800", "1765"),
+    preguntas("Who was the leader of the Russian Revolution in 1917?", "Joseph Stalin", "Vladimir Lenin", "Leon Trotsky", "Nikita Khrushchev"),
+    preguntas("Which battle ended the American Civil War?", "Battle of Gettysburg", "Battle of Antietam", "Battle of Bull Run", "Battle of Appomattox"),
+    preguntas("When did the French Revolution take place?", "1776", "1789", "1848", "1765"),
+    preguntas("What was the ancient capital of the Roman Empire?", "Athens", "Rome", "Constantinople", "Paris"),
+    preguntas("In what year was the League of Nations founded?", "1919", "1925", "1933", "1908"),
     preguntas("Who was the leader of the Indian resistance against British rule?", "Jawaharlal Nehru", "Mohandas Gandhi", "Subhas Chandra Bose", "Bhagat Singh")
 
 ]
