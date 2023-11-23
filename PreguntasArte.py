6382c982a4dbbe0dd6306ca293bdbd53822cd1a1
Pesqueraa <145668210+Pesqueraa@users.noreply.github.com>
2023-11-23 13:19:33 +0100

    Create PreguntasArte.py

---
+++ PreguntasArte.py
@@ -0,0 +1,15 @@
+import random
+from ClasePreguntas import preguntas
+art_question = [
+    preguntas("¿Quién esculpió 'El Pensador'?", "Rodin", "Miguel Angel", "Bernini", "Moore"),
+    preguntas("¿Quién pintó La última cena durante un período de tres años entre 1495 y 1498?", "Leonardo da Vinci", "Michaelangelo", "Botticelli", "Rafael"),
+    preguntas("¿Qué artista es famoso por sus coloridas representaciones de la vida nocturna de París?", "Manet", "Toulouse-Lautrec", "Dubuffet", "mucha"),
+    preguntas("¿Cuál de estos pintores no es italiano?", "Pablo Picasso", "Tiziano", "Leonardo da Vinci", "Caravaggio"),
+    preguntas("¿Qué artista pintó 'La ronda de noche'?", "Rembrandt", "Gainsborough", "van eyck", "Rubens"),
+    preguntas("¿Cuál de estos artistas usó términos musicales como “nocturno” y “armonía” para describir sus cuadros?", "James Whistler", "Leonardo da Vinci", "Edgar Degas", "Vincent van Gogh"),
+    preguntas("¿A qué escuela de pintura pertenecen las pinturas de Salvador Dalí?", "Surrealismo", "Modernismo", "Realismo", "Impresionismo"),
+    preguntas("¿Dónde se encuentra 'La Última Cena' de Leonardo da Vinci?", "La Santa Maria Delle Grazie en Milán, Italia", "El Louvre en París, Francia", " El Museo Metropolitano de la ciudad de Nueva York", "La Galería de Los Ufizzi de Florencia, Italia"),
+    preguntas("¿Qué artista instaló “The Gates” en el Central Park de Nueva York en 2005?", "Christo", "Jasper Johns", "David Hockney", "Ninguna de las anteriores"),
+    preguntas("Miguel Ángel creó todas las siguientes obras de arte EXCEPTO?", "La guardia nocturna", "El techo de la Capilla Sixtina", "La escultura david", "El juicio final"),
+
+]
